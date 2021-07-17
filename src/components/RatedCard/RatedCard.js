afa522f27fdbce0a12a8a4798b4ab71558b56661
smiri99 <56261918+smiri99@users.noreply.github.com>
2021-07-17 22:32:36 +0100

    ratedcard

--- src/components/RatedCard/RatedCard.js
+++ src/components/RatedCard/RatedCard.js
@@ -3,6 +3,8 @@ import styles from './RatedCard.module.css';
 
 const RatedCard = () => (
   <div className={styles.RatedCard}>
+    <h1>Gilles D. (Ixelles)</h1>  
+    <h2>&#9733; &#9733; &#9733; &#9733; &#9733;</h2>
     <p>L’équipe Agorim est disponible, professionnelle, pleine de ressources et efficace. Ils nous ont accompagnés tout au long de la vente de notre appartement. En plus d’avoir fait ce qu’on attend d’une agence immobilière, ils nous ont apporté leur expertise juridique. On ne peut que la conseiller.</p>
   </div>
 );
